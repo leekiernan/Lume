08ed9058fe3b0b86f6dff6638e0a3fe9c34bc8bc
leekiernan <dev@starlord.co>
2026-09-20 19:08:55 +0100

    Fix browse test fixture optional ordering

--- LumeTests/Services/BrowseQueryShapeTests.swift
+++ LumeTests/Services/BrowseQueryShapeTests.swift
@@ -493,7 +493,7 @@ struct BrowseQueryShapeTests {
 
 private protocol FavoriteHomeFixture: AnyObject {
     var isFavorite: Bool { get set }
-    var favoriteOrder: Int { get set }
+    var favoriteOrder: Int? { get set }
     var lastWatchedDate: Date? { get set }
 }
 
